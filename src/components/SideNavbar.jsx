c437456d3e1de80e3b2939b7a68761d9d62b59ce
Betsue Weldemariam <btsegay0@gmail.com>
2023-05-02 11:03:28 +0300

    Added a final touch to make it look good

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -1,9 +1,9 @@
-import {AnimatePresence, easeIn, easeInOut, motion} from 'framer-motion'
+import {AnimatePresence, motion} from 'framer-motion'
 import { useState } from 'react';
 import HamburgerMenu from '../assets/hamburger-menu.png'
 import x from '../assets/x.png'
 
-function SideNavbar ({backdrop}) {
+function SideNavbar () {
 
   const [clicked, setClicked] = useState(false)
 
@@ -13,38 +13,26 @@ function SideNavbar ({backdrop}) {
       <div className='nav-container'>
         <motion.div whileHover={{cursor: "pointer"}} onClick={() => setClicked(!clicked)} className='btn-container'><img className='open-btn' src={HamburgerMenu}/></motion.div>
         <AnimatePresence>
-        {clicked && <motion.div className='side-navbar' 
-        initial={{x: "100vw"}}
-        animate={{x: 0}}
-        exit={{x: "100vw"}}
-        transition={{duration: 0.5, type: "tween"}}>
+        {(clicked && 
+
+        <motion.div className='parent'>
+          <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} onClick={() => setClicked(!clicked)} className='backdrop'></motion.div>
+          <motion.div className='side-navbar' 
+            initial={{x: "100vw"}}
+            animate={{x: 0}}
+            exit={{x: "100vw"}}
+            transition={{duration: 0.5, type: "tween"}}>
           <motion.div whileHover={{cursor: "pointer"}} className='close-btn-pos' onClick={() => setClicked(!clicked)}><img className='close-btn' src={x} /></motion.div>
-          <motion.ul
-            whileHover={{}}
-          >
-            <motion.li
-              initial={{textDecoration:"none"}}
-              whileHover={{textDecoration: "underline",cursor: "pointer", transition: {type: "spring", duration: 0.5}}}
-              transition={{duration: 0.5, type: "tween"}}
-            >Home</motion.li>
-            <motion.li 
-              whileHover={{textDecoration: "underline", cursor: "pointer"}}
-              transition={{duration: 0.5, type: "tween"}}
-            >Clothing</motion.li>
-            <motion.li
-              whileHover={{textDecoration: "underline", cursor: "pointer"}}
-              transition={{duration: 0.5, type: "tween"}}
-            >Grooming</motion.li>
-            <motion.li
-              whileHover={{textDecoration: "underline", cursor: "pointer"}}
-              transition={{duration: 0.5, type: "tween"}}
-            >Accessories</motion.li>
-            <motion.li
-              whileHover={{textDecoration: "underline", cursor: "pointer"}}
-              transition={{duration: 0.5, type: "tween"}}
-            >Shoes</motion.li>
+          <motion.ul>
+            <motion.li>Home</motion.li>
+            <motion.li>Clothing</motion.li>
+            <motion.li>Grooming</motion.li>
+            <motion.li>Accessories</motion.li>
+            <motion.li>Shoes</motion.li>
           </motion.ul>
-          </motion.div>}
+          </motion.div>
+        </motion.div>      
+        )}
           </AnimatePresence>
       </div>
     </div>
